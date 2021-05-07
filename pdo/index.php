70e8936cea672b6a7aac2e562b3e944827696a82
Jefferson Mateus <jeffersonmateus64@gmail.com>
2021-05-07 16:37:50 -0300

    Tratamento de exeções com try/catch e PDOException

--- pdo/index.php
+++ pdo/index.php
@@ -1,3 +1,25 @@
 <?php
-	echo "Hello, Word!";
-?>
+    //Aula atual   -> Aula 06 Tratamento de exeções  
+    //Próxima aula -> Aula 07
+
+    /* Curso de conexão com banco de dados com PDO - Fomação PHP
+     * Aluno: Jefferson Mateus
+     * Prof: Manoel Jailton
+     */
+
+    //Tratamento de exeções com try e catch
+
+    try {//Oque deve ser executado
+        
+    $conexao = new \PDO("mysql:host=localhost;dbname=estudoPDO","root",     "");//Criando conexao com o banco
+    //A conexao deve receber o banco, host,    nome do banco,    usuario, e senha
+      
+    } catch (PDOException $e) {//Oque deve ser executado caso tenha algum erro (Exeção)
+        
+        echo'Não foi possivel conectar com o banco de dados :(</br>Erro '.$e->getCode()."</br>";
+        //Mensagem de erro personalisada + a do metado getCode que retorna o codigo do erro
+    }
+
+    
+    echo "Hello, Word!";
+
