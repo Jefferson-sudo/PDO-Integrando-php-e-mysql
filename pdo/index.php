6378011c3155f9d8f6b118ef581a7b6c55578252
Jefferson Mateus <jeffersonmateus64@gmail.com>
2021-05-14 17:01:53 -0300

    Exec com select e delet

--- pdo/index.php
+++ pdo/index.php
@@ -12,9 +12,18 @@
     $conexao = new \PDO("mysql:host=localhost;dbname=estudoPDO","root",     "");//Criando conexao com o banco
     //A conexao deve receber o banco, host,    nome do banco,    usuario, e senha
     
-   // $comando = "insert into cliente (nome, email) values ('Mateus', 'Mateus@mateus.com.br')"
-    $comando = "update cliente set ativo='S' where nome like '%a%'";
-    $resultado = $conexao ->exec($comando);//O comando sql e passado para a conexã. Exec observa quantas linhas foram aferadas
+    
+    /* Estudando o exec 
+     *  O metado exec retorna a quantidade de linhas que foram afetadas no banco de dados  
+     */
+    
+    //$comando = "insert into cliente (nome, email) values ('Mateus', 'Mateus@mateus.com.br')"
+
+    //$comando = "update cliente set ativo='S' where nome like '%a%'"; //Atige na coluna 'ativo' as linhas que possuem no 'nome' a letra a
+    
+    $comando = "delete from cliente where email='jefferson@jefferson.com.br'"; //Deleta toda linha que possui esse endereco
+    
+    $resultado = $conexao ->exec($comando);//O comando sql e passado para a conexão. Exec observa quantas linhas foram afetadas pelo comando
     
     //Imprime resultado
     echo "Number of rows affected ".$resultado."</br>";
