fda0dae589114620264e88c40234406eb721a592
Jefferson Mateus <jeffersonmateus64@gmail.com>
2021-05-18 17:04:28 -0300

    Configurando constantes com as informações para fazer a conexão com o BD

--- crudGenericoPdo/config.php
+++ crudGenericoPdo/config.php
@@ -1,4 +1,9 @@
 <?php
-//Guardando informações do banco de dados nas constantes Keeping the database information in the constants
-define("SERVIDOR","localhost"); define("USUARIO","host"); define("SENHA","");
-define("BANCO","crudpdo");
+
+//Guardando informações do banco de dados nas constantes 
+//Keeping the database information in the constants
+
+define("SERVIDOR", "localhost");
+define("USUARIO", "host");
+define("SENHA", "");
+define("BANCO", "crudpdo");
