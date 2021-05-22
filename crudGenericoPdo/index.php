18376a365af046691fa1fd5318c7add092081c07
Jefferson Mateus <jeffersonmateus64@gmail.com>
2021-05-22 09:36:29 -0300

    Testando a função inserir

--- crudGenericoPdo/index.php
+++ crudGenericoPdo/index.php
@@ -1,9 +1,15 @@
 <?php
-/* Aula 05 Testando a classe DB
- */
 
-include('DB.php'); //Include é uma função do PHP que permite a inclusão do conteúdo de um outro arquivo no arquivo PHP atual
+// Aula 05 Testando a classe DB
+include('DB.php'); 
+include('crudPDO.php');
+//Include é uma função do PHP que permite a inclusão do conteúdo de um outro arquivo no arquivo PHP atual
 
-$conn = DB::getInstance(); //Abre conexão com o BD chamando o metado estatico da classe DB
-var_dump($conn);
+//$conn = DB::getInstance(); //Abre conexão com o BD chamando o metado estatico da classe DB
 
+
+$dados = array ("nome"=> "Pedro Homem", "email"=> "pedrohomem@pedrohomem.com", "fone"=> "31331311");
+print_r(inserir("cliente", $dados));
+//print_r(alterar("cliente", $dados, "id_cliente = 2"));
+
+?>
