89b50d7d1ca46cc186a4dde707abc4a56bc9b706
Jefferson Mateus <jeffersonmateus64@gmail.com>
2021-05-24 16:12:41 -0300

    Comentarios

--- pdo/index2.php
+++ pdo/index2.php
@@ -17,7 +17,7 @@ try{//What should be performed
    
    $sql = "INSERT INTO produto (produto, preco)values (?, ?)";
    $statement = $conexao->prepare($sql);//Prepare SQL
-   $statement->bindValue(1,$produto);//Faz com que o '?' na linha 17 seja substituido pela variavel $id --Sql injection handling.
+   $statement->bindValue(1,$produto);//Faz com que o '?' na linha 18 seja substituido pela variavel $produto --Sql injection handling.
    $statement->bindValue(2, $preco);
    
    //Imprime tudo da tabela
